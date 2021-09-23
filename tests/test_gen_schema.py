e2cc035949330261f95f83613c3b55ed61d81dfc
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2021-09-23 16:57:46 -0400

    Remove code for testing

--- tests/test_gen_schema.py
+++ tests/test_gen_schema.py
@@ -8,7 +8,6 @@ from pydata_factory.schema import create_schema
 
 @pytest.mark.parametrize("filename", ["fb2021.parquet", "msft2021.parquet"])
 def test_create_schema(filename):
-    return
     """Test the creation of a new model from a parquet file."""
     origin = Path(__file__).parent / "data" / "original" / filename
     target_dir = Path(__file__).parent / "data" / "schemas"
